3c2439999645d408e91fe2af1f46f7624358d99b
Mr-LiuDC <442823529@qq.com>
2018-05-15 07:32:02 +0800

    测试CI效果

--- App.js
+++ App.js
@@ -46,8 +46,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    // backgroundColor: '#abc123',
+    // backgroundColor: '#F5FCFF',
+    backgroundColor: '#abc123',
   },
   welcome: {
     fontSize: 20,
